79f98359a44a14dc0ce67227cc46f92a1e7ba885
migichan3ist <migichan@ist.hokudai.ac.jp>
2022-10-23 14:26:20 +0900

    ほぼ完成

--- js/index.js
+++ js/index.js
@@ -5,12 +5,12 @@ document.getElementById("move_camera").onclick = function() {
     location.href = "./camera.html?id=" + userId;
 };
 
-document.getElementById("move_bet").onclick = function() {
-    const url = new URL(window.location.href);
-    const params = url.searchParams;
-    const userId = params.get("id");
-    location.href = "./bet.html?id=" + userId;
-};
+// document.getElementById("move_bet").onclick = function() {
+//     const url = new URL(window.location.href);
+//     const params = url.searchParams;
+//     const userId = params.get("id");
+//     location.href = "./bet.html?id=" + userId;
+// };
 
 document.getElementById("move_conform").onclick = function() {
     const url = new URL(window.location.href);
